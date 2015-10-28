a6913ac9bf7e53f46a832e430386b051c93e03ae
Onur Sumer <s.onur.sumer@gmail.com>
2015-10-28 19:32:36 -0400

    initial commit

---
+++ src/main/script/PortalWebService.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sos on 10/28/15.
+ */
+
+var PortalWebService = function(baseUrl)
+{
+	var _baseUrl = baseUrl || "http://www.cbioportal.org/private/webservice.do";
+
+	// TODO this implementation requires to use google authentication (oAuth2)
+};
